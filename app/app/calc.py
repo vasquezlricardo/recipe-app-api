f46ad9c0544281bad67a26c2dc20da5489a4f78a
vasquezlricardo <ricardo.vasquez.lizama@gmail.com>
2022-11-25 12:11:30 -0300

    Adding core app to avoid race conditions

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtracting x and y and return result."""
     return x - y
