cf4f59b489f0ae0b95cbcceeb575911ca2a3559b
vasquezlricardo <ricardo.vasquez.lizama@gmail.com>
2022-11-26 01:00:02 -0300

    fixing linter

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -67,7 +67,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_for_user(self):
         """Test generates token for valid credentials"""
-        user_details={
+        user_details = {
             'name': 'Test Name',
             'email': 'test@example.com',
             'password': 'test-user-password123',
@@ -87,7 +87,7 @@ class PublicUserApiTests(TestCase):
         """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
 
-        payload = {'email':'test@example.com', 'password': 'badpass'}
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
@@ -95,7 +95,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_token_blank_password(self):
         """Test returns error if credentials invalid."""
-        payload = {'email':'test@example.com', 'password': ''}
+        payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
         self.assertNotIn('token', res.data)
