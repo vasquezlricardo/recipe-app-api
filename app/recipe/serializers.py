94951997790699c37d437da318b2c87e04e7369a
vasquezlricardo <ricardo.vasquez.lizama@gmail.com>
2022-11-28 10:45:39 -0300

    fixing linter

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,10 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags', 'ingredients']
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -57,7 +60,6 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **ingredient,
             )
             recipe.ingredients.add(ingredient_obj)
-
 
     def create(self, validated_data):
         """Create a recipe"""
